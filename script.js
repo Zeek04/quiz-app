74c37f2fe948e5da63f60ee5b24bd1bf08594e18
Ezequiel <cruzezequiel81@gmail.com>
2025-01-22 23:19:07 -0500

    Added a couple of functions that will check answer and shuffle answers.

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
-const API_URL = 'https://opentdb.com/api.php?amount=1&category=27&difficulty=easy&type=multiple';
+const API_URL =
+  "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple";
 
 async function fetchQuestion() {
-    try{
-        const response = await fetch(API_URL);
-        const data = await response.json();
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
 
-        const questionData = data.results[0]
-        const questions = questionData.question; //Will get question data from api//
-        const correctAnswer = questionData.correct_answer;  //Will get correct answer data from api//
-        const incorrectAnswers = questionData.incorrect_answers;    //Will get incorrect answer data from api//
+    const questionData = data.results[0];
+    const questions = questionData.question; //Will get question data from api//
+    const correctAnswer = questionData.correct_answer; //Will get correct answer data from api//
+    const incorrectAnswers = questionData.incorrect_answers; //Will get incorrect answer data from api//
+    const allAnswers = shuffleArray([correctAnswer, ...incorrectAnswers]); //Combine the correct answer data with the incorrect answer array//
 
-
-    }catch{
-        console.error("Error", error);
-    }
+    displayQuestion(questions, allAnswers, correctAnswer);
+  } catch {
+    console.error("Error", error);
+  }
 }
 
 //Function to display each answer//
-function displayQuestion(question, answers){                       
-    const questionElement = document.getElementById("question")
-    const answersElement = document.getElementById('answer')
+function displayQuestion(question, answers, correctAnswer) {
+  const questionElement = document.getElementById("question");
+  const answersElement = document.getElementById("answer");
 
-    questionElement.innerHTML = question
+  questionElement.innerHTML = question;
 
-    answersElement.innerHTML = "";
+  answersElement.innerHTML = "";
 
-    //Will create an li & Button element for each answer option fetch from data//
-    answers.forEach(answer => {
-        const li = document.createElement('li')
-        const button = document.createElement('button')
-
-        button.innerText = answer
-
-        li.appendChild(button)
-        answersElement.appendChild(li)
-
-    });
-    
+  //Will create an li & Button element for each answer option fetch from data//
+  answers.forEach((answer) => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.innerText = answer;
+    button.onclick = () => checkAnswer(answer, correctAnswer);
+    li.appendChild(button);
+    answersElement.appendChild(li);
+  });
 }
 
-fetchQuestion()
+function checkAnswer(selectedAnswer, correctAnswer) {
+  if (selectedAnswer === correctAnswer) {
+        alert("Correct!");
+  } else {
+        alert("Incorrect Try Again!");
+  }
+
+  fetchQuestion();
+}
+
+function shuffleArray(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+fetchQuestion();
